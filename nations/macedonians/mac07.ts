e5992886896d556bcf35794d96f479d2403deb6c
Boris Yankov <borisyankov@gmail.com>
2022-08-21 07:35:21 +0300

    Add tests for Macedonians and fix cards

--- nations/macedonians/mac07.ts
+++ nations/macedonians/mac07.ts
@@ -1,7 +1,8 @@
-import { Card, Nation, StartingLocation, State } from '../../types';
+import { Card, Icon, Nation, StartingLocation, State } from '../../types';
 
 const mosaics: Card = {
   cardName: 'Mosaics',
+  icon: Icon.PINNED,
   state: State.EMPIRE,
   effect: 'Exhaust: pay 1 {materials} to draw a card from your discard pile.',
   nation: Nation.MACEDONIANS,
--- nations/macedonians/mac11.ts
+++ nations/macedonians/mac11.ts
@@ -1,7 +1,8 @@
-import { Card, Nation, StartingLocation, State, Suit } from '../../types';
+import { Card, Icon, Nation, StartingLocation, State, Suit } from '../../types';
 
 const uxians: Card = {
   cardName: 'Uxians',
+  icon: Icon.ATTACK,
   state: State.EMPIRE,
   effect: 'Steal 2 {materials} from each other player. You MAY return an {exhaust} to your state card.',
   suit: Suit.TRIBUTARY,
